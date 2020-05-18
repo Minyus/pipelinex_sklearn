e354a9d1813a2c908bda96c08a9dcabacc8ca84d
Minyus <yusuke.minami86@gmail.com>
2020-05-18 10:14:16 +0800

    Update for the latest PipelineX

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from pipelinex import __version__, configure_paths, FlexibleContext
+from pathlib import Path
+from pipelinex import __version__, configure_source, load_context
 
 
-print("pipelinex version: ", __version__)
-
-project_path, src_path = configure_paths(__file__)
-print("project path: ", project_path)
-print("src path: ", src_path)
-
-context = FlexibleContext(project_path)
-context.run()
+if __name__ == "__main__":
+    print("pipelinex version: ", __version__)
+    project_path = Path(__file__).resolve().parent
+    print("project path: ", project_path)
+    source_path = configure_source(project_path)
+    print("source path: ", source_path)
+    context = load_context(project_path)
+    context.run()
